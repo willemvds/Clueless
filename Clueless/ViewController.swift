b76782969995b495bbe74d03dc5b9a2c48f87be9
Willem van der Schyff <willemvds@gmail.com>
2014-10-21 21:43:41 +0200

    Something? iThink!

--- Clueless/ViewController.swift
+++ Clueless/ViewController.swift
@@ -7,8 +7,27 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController {
+    @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchResultLabel: UILabel!
+    @IBOutlet weak var searchResultTableView: UITableView!
+
+    @IBAction func findClick(sender: UIButton) {
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://poco.cloudapp.net/api/locations/7/"))
+        var response:NSURLResponse?
+        var error:NSError?
+
+        let result:NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
+        let httpResponse:NSHTTPURLResponse? = response as? NSHTTPURLResponse
+
+        var datastring = NSString(data: result!, encoding: NSUTF8StringEncoding)
+        searchResultLabel.text = datastring
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
