4a6785f16abb6e4720d82a73be7a03826a8e4183
Willem van der Schyff <willemvds@gmail.com>
2014-10-21 19:40:20 +0200

    Initial Commit

---
+++ Clueless/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clueless
+//
+//  Created by Willem van der Schyff on 2014/10/21.
+//  Copyright (c) 2014 Willem van der Schyff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
